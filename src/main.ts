d091af802f04111a706b38882d3616430f65419f
Rob Wormald <robwormald@gmail.com>
2015-12-11 05:49:09 -0800

    move deps

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import 'reflect-metadata';
-import 'zone.js'
-
 import {bootstrap} from 'angular2/platform/browser';
+import {platform, provide} from 'angular2/core';
+import {AuthService} from './services/Auth';
+import {Backend, BackendConfig} from './services/Backend';
+import {Nav} from './services/Nav';
+
+const FIREBASE_URL = 'https://ng2-forum-demo.firebaseio.com';
+
 import {App} from './app/app';
 
-bootstrap(App);
+bootstrap(App,[Nav, AuthService, Backend, provide(BackendConfig, {useValue: {url: FIREBASE_URL }})]);
+
