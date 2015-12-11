e69a9f0a1f375737de615ddd16a78a3b4e792ee4
Rob Wormald <robwormald@gmail.com>
2015-12-11 05:47:51 -0800

    system config

--- system.config.js
+++ system.config.js
@@ -1,32 +1,27 @@
 System.config({
 		transpiler: 'typescript',
 		typescriptOptions: {
-		emitDecoratorMetadata: true
-		},
+    	emitDecoratorMetadata: true
+  	},
 		map: {
-		typescript: 'node_modules/typescript/lib/typescript.js',
-		app: './src',
-		angular2: 'node_modules/angular2',
-		rxjs: 'node_modules/rxjs',
-		'reflect-metadata': 'node_modules/reflect-metadata/temp/Reflect.js',
-		'zone.js': 'node_modules/zone.js/dist/zone.js'
+			typescript: 'node_modules/typescript/lib/typescript.js',
+			app: './src',
+			angular2: 'node_modules/angular2',
+			rxjs: 'node_modules/rxjs',
+			'reflect-metadata': 'node_modules/reflect-metadata/temp/Reflect.js',
+			'zone.js': 'node_modules/zone.js/dist/zone.js',
+			firebase: 'node_modules/firebase/lib/firebase-web.js'
 		},
 		packages: {
-		app: {
-			defaultExtension: 'ts',
-			main: 'main.ts'
-		},
-		angular2: {
-			defaultExtension: 'js',
-		},
-		rxjs: {
-			defaultExtension: 'js'
-		},
-		'reflect-metadata': {
-			format: 'global'
-		},
-		'zone.js': {
-			format: 'global'
-		}
+			app: {
+				defaultExtension: 'ts',
+				main: 'main.ts'
+			},
+			angular2: {
+				defaultExtension: 'js',
+			},
+			rxjs: {
+				defaultExtension: 'js'
+			}
 		}
 });
