0fae3c9b47b5fbe0cc2e7c5c7ff47121124a5061
Rob Wormald <robwormald@gmail.com>
2015-12-11 00:13:07 -0800

    systemJS config

---
+++ system.config.js
@@ -0,0 +1,32 @@
+System.config({
+		transpiler: 'typescript',
+		typescriptOptions: {
+		emitDecoratorMetadata: true
+		},
+		map: {
+		typescript: 'node_modules/typescript/lib/typescript.js',
+		app: './src',
+		angular2: 'node_modules/angular2',
+		rxjs: 'node_modules/rxjs',
+		'reflect-metadata': 'node_modules/reflect-metadata/temp/Reflect.js',
+		'zone.js': 'node_modules/zone.js/dist/zone.js'
+		},
+		packages: {
+		app: {
+			defaultExtension: 'ts',
+			main: 'main.ts'
+		},
+		angular2: {
+			defaultExtension: 'js',
+		},
+		rxjs: {
+			defaultExtension: 'js'
+		},
+		'reflect-metadata': {
+			format: 'global'
+		},
+		'zone.js': {
+			format: 'global'
+		}
+		}
+});
