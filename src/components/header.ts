f71fb7cdcfe55382d2151bd0bd3d1fee429d8c6e
Rob Wormald <robwormald@gmail.com>
2015-12-11 05:48:47 -0800

    add header

---
+++ src/components/header.ts
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core';
+import {Nav} from '../services/Nav';
+
+@Component({
+	selector: 'app-header',
+	template: `
+		<img role="tab" class="header__menu js-toggle-menu" src="/images/ic_menu_24px.svg" alt="toggle_nav" (click)="openSideNav()"/>
+
+		<h1 class="header__title">Ng2 Shell</h1>
+	`
+})
+export class AppHeader {
+	constructor(private nav: Nav){
+		
+	}
+	openSideNav(){
+		this.nav.open()
+	}
+}
