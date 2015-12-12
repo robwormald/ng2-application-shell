f1e051c5bccb5a58a1f10f07d2cc67a0eff028d3
Rob Wormald <robwormald@gmail.com>
2015-12-12 05:59:16 -0800

    formatting

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,20 @@
+//ng2 deps
 import {Component} from 'angular2/core';
 import {Router, Route, RouteConfig, AsyncRoute, ROUTER_DIRECTIVES} from 'angular2/router';
 
-
+//app services
 import {AuthService} from '../services/Auth';
 import {Nav} from '../services/Nav';
 import {Backend, BackendConfig} from '../services/Backend';
 
+//generic components
 import {AppHeader} from '../components/header';
 import {SideNav} from '../components/sidenav';
 import {Toast} from '../components/toast';
 
+//routable components
 import {Home} from '../components/Home';
 import {About} from '../components/About';
-
-
 
 @Component({
 	selector: 'app',
@@ -28,16 +29,14 @@ import {About} from '../components/About';
 		<side-nav></side-nav>`,
 })
 @RouteConfig([
-	new Route({name: 'Home', component:Home, path: '/home', useAsDefault: true}),
-	new Route({name: 'About', component: About, path: '/about'})
+	new Route({ name: 'Home', component: Home, path: '/home', useAsDefault: true }),
+	new Route({ name: 'About', component: About, path: '/about' })
 ])
 export class App {
-	constructor(public backend: Backend, public nav: Nav){
-		
-	}
-	
-	authenticate(){
+	constructor(public backend: Backend, public nav: Nav) { }
+
+	authenticate() {
 		this.backend.authenticate()
 	}
-	showToast(message){}
+	showToast(message) { }
 }
