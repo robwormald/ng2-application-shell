63946249cfed80aa665cc081d80e7f75a214e322
Rob Wormald <robwormald@gmail.com>
2015-12-12 05:53:42 -0800

    add sw compilation

--- tasks/compile.ts
+++ tasks/compile.ts
@@ -1,16 +1,17 @@
 declare var require;
 
 var Builder = require('systemjs-builder');
+var util = require('gulp-util');
 
 export const compile = (gulp, config) => {
 	
 	gulp.task('compile:app', ['compile:vendor'], () => {
 		
 		let builder = new Builder();
-		
+
 		return builder.loadConfig(config.system.configFile)
 		  .then(() => {
-				return builder.bundle('app - dist/vendor.js', 'dist/app.js', {minify: true})
+				return builder.bundle('app - dist/vendor.js', 'dist/app.js', {minify: util.env.production})
 			})
 	});
 
@@ -20,7 +21,17 @@ export const compile = (gulp, config) => {
 
 		return builder.loadConfig(config.system.configFile)
 		  .then(() => {
-				return builder.bundle('app - [app/**/*]', 'dist/vendor.js', {minify: true, mangle: true})
+				return builder.bundle('app - [app/**/*]', 'dist/vendor.js', {minify: util.env.production})
+			})
+	});
+
+	gulp.task('compile:serviceworker', [], () => {
+
+		let builder = new Builder();
+
+		return builder.loadConfig(config.system.configFile)
+		  .then(() => {
+				return builder.buildStatic('app/ng2-service-worker', 'dist/worker.js', {minify: util.env.production})
 			})
 	});
 }
